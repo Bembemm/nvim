4c0cfab31c74ec357eb3f3ceb55005753a8f86ab
brenoqwq <89336218+Bembemm@users.noreply.github.com>
2026-08-13 15:59:55 -0300

    refactor: use conform formatter defaults

--- plugin/conform.lua
+++ plugin/conform.lua
@@ -1,23 +1,10 @@
 local conform = require("conform")
 
-local formatters = {}
-local stylua = vim.fn.exepath("stylua")
-local clang_format = vim.fn.exepath("clang-format")
-
-if stylua ~= "" then
-    formatters.stylua = { command = stylua }
-end
-
-if clang_format ~= "" then
-    formatters["clang-format"] = { command = clang_format }
-end
-
 conform.setup({
     formatters_by_ft = {
         lua = { "stylua" },
         cpp = { "clang-format" },
     },
-    formatters = formatters,
     default_format_opts = {
         lsp_format = "fallback",
     },
