8b9e45283289cf8f07fa096f3d217735620e5c0f
brenoqwq <89336218+Bembemm@users.noreply.github.com>
2026-08-13 14:53:20 -0300

    refactor: remove executable helper from conform

--- plugin/conform.lua
+++ plugin/conform.lua
@@ -1,29 +1,15 @@
-local executable = require("core.executable")
-
 local conform = require("conform")
 
 local formatters = {}
+local stylua = vim.fn.exepath("stylua")
+local clang_format = vim.fn.exepath("clang-format")
 
-local stylua_cmd = executable.find({
-    names = "stylua",
-    env = "STYLUA_PATH",
-})
-
-if executable.available(stylua_cmd) then
-    formatters.stylua = { command = stylua_cmd }
+if stylua ~= "" then
+    formatters.stylua = { command = stylua }
 end
 
-local clang_format_cmd = executable.find({
-    names = "clang-format",
-    env = "CLANG_FORMAT_PATH",
-    extra_paths = {
-        "/opt/homebrew/opt/llvm/bin/clang-format",
-        "/usr/local/opt/llvm/bin/clang-format",
-    },
-})
-
-if executable.available(clang_format_cmd) then
-    formatters["clang-format"] = { command = clang_format_cmd }
+if clang_format ~= "" then
+    formatters["clang-format"] = { command = clang_format }
 end
 
 conform.setup({
