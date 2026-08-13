310966433e1fc14ad18ebd7ff483df89d6e68a94
brenoqwq <89336218+Bembemm@users.noreply.github.com>
2026-08-13 14:07:53 -0300

    refactor: prepare conform for optional formatters

--- plugin/conform.lua
+++ plugin/conform.lua
@@ -2,33 +2,36 @@ local executable = require("core.executable")
 
 local conform = require("conform")
 
+local formatters = {}
+
+local stylua_cmd = executable.find({
+    names = "stylua",
+    env = "STYLUA_PATH",
+})
+
+if stylua_cmd then
+    formatters.stylua = { command = stylua_cmd }
+end
+
+local clang_format_cmd = executable.find({
+    names = "clang-format",
+    env = "CLANG_FORMAT_PATH",
+    extra_paths = {
+        "/opt/homebrew/opt/llvm/bin/clang-format",
+        "/usr/local/opt/llvm/bin/clang-format",
+    },
+})
+
+if clang_format_cmd then
+    formatters["clang-format"] = { command = clang_format_cmd }
+end
+
 conform.setup({
     formatters_by_ft = {
         lua = { "stylua" },
         cpp = { "clang-format" },
     },
-    formatters = {
-        stylua = {
-            command = function()
-                return executable.find({
-                    names = "stylua",
-                    env = "STYLUA_PATH",
-                })
-            end,
-        },
-        ["clang-format"] = {
-            command = function()
-                return executable.find({
-                    names = "clang-format",
-                    env = "CLANG_FORMAT_PATH",
-                    extra_paths = {
-                        "/opt/homebrew/opt/llvm/bin/clang-format",
-                        "/usr/local/opt/llvm/bin/clang-format",
-                    },
-                })
-            end,
-        },
-    },
+    formatters = formatters,
     default_format_opts = {
         lsp_format = "fallback",
     },
