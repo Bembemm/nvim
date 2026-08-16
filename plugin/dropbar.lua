b9199492e454c9845af804405a17dc0f135c8487
brenoqwq <89336218+Bembemm@users.noreply.github.com>
2026-08-16 01:55:30 -0300

    make dropbar startup compatible during updates

--- plugin/dropbar.lua
+++ plugin/dropbar.lua
@@ -1,7 +1,20 @@
 local dropbar = require("dropbar")
 local dropbar_api = require("dropbar.api")
 
-dropbar.setup()
+-- Keep startup compatible with older local Dropbar checkouts while vim.pack
+-- updates them to the current default branch. BufModifiedSet was removed in
+-- Neovim 0.13, so do not rely on that event here.
+dropbar.setup({
+    bar = {
+        update_events = {
+            buf = {
+                "FileChangedShellPost",
+                "TextChanged",
+                "ModeChanged",
+            },
+        },
+    },
+})
 
 vim.keymap.set("n", "<leader>;", dropbar_api.pick, {
     desc = "Dropbar escolher contexto",
