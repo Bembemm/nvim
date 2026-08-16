e33adb129c1066b005b861931d2999f836df8749
brenoqwq <89336218+Bembemm@users.noreply.github.com>
2026-08-15 22:34:47 -0300

    configure dropbar breadcrumbs

---
+++ plugin/dropbar.lua
@@ -0,0 +1,13 @@
+local dropbar_api = require("dropbar.api")
+
+vim.keymap.set("n", "<leader>;", dropbar_api.pick, {
+    desc = "Dropbar escolher contexto",
+})
+
+vim.keymap.set("n", "[;", dropbar_api.goto_context_start, {
+    desc = "Dropbar início do contexto",
+})
+
+vim.keymap.set("n", "];", dropbar_api.select_next_context, {
+    desc = "Dropbar próximo contexto",
+})
