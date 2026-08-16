1180e6069776de9729568735044b6085f2b46480
brenoqwq <89336218+Bembemm@users.noreply.github.com>
2026-08-16 01:52:16 -0300

    use latest dropbar defaults

--- plugin/dropbar.lua
+++ plugin/dropbar.lua
@@ -1,23 +1,7 @@
 local dropbar = require("dropbar")
 local dropbar_api = require("dropbar.api")
 
-local buf_update_events = {
-    "FileChangedShellPost",
-    "TextChanged",
-    "ModeChanged",
-}
-
-if vim.fn.exists("##BufModifiedSet") == 1 then
-    table.insert(buf_update_events, 1, "BufModifiedSet")
-end
-
-dropbar.setup({
-    bar = {
-        update_events = {
-            buf = buf_update_events,
-        },
-    },
-})
+dropbar.setup()
 
 vim.keymap.set("n", "<leader>;", dropbar_api.pick, {
     desc = "Dropbar escolher contexto",
