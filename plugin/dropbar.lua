597739b313467bcd96c464f521ad140a986ab101
brenoqwq <89336218+Bembemm@users.noreply.github.com>
2026-08-16 01:48:04 -0300

    fix dropbar event compatibility

--- plugin/dropbar.lua
+++ plugin/dropbar.lua
@@ -1,4 +1,23 @@
+local dropbar = require("dropbar")
 local dropbar_api = require("dropbar.api")
+
+local buf_update_events = {
+    "FileChangedShellPost",
+    "TextChanged",
+    "ModeChanged",
+}
+
+if vim.fn.exists("##BufModifiedSet") == 1 then
+    table.insert(buf_update_events, 1, "BufModifiedSet")
+end
+
+dropbar.setup({
+    bar = {
+        update_events = {
+            buf = buf_update_events,
+        },
+    },
+})
 
 vim.keymap.set("n", "<leader>;", dropbar_api.pick, {
     desc = "Dropbar escolher contexto",
