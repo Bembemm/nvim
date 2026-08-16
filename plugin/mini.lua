04428e6faeac11d6ecf1b0a34143bbd78afab19b
brenoqwq <89336218+Bembemm@users.noreply.github.com>
2026-08-15 21:31:53 -0300

    enable mini jump2d navigation

--- plugin/mini.lua
+++ plugin/mini.lua
@@ -1,5 +1,51 @@
 local pairs = require("mini.pairs")
+local jump2d = require("mini.jump2d")
 
 pairs.setup({
     modes = { insert = true, command = false, terminal = false },
+})
+
+jump2d.setup({
+    view = {
+        dim = false,
+        n_steps_ahead = 1,
+    },
+    mappings = {
+        start_jumping = "",
+    },
+})
+
+local modes = { "n", "x", "o" }
+
+vim.keymap.set(
+    modes,
+    "<leader>jj",
+    "<Cmd>lua MiniJump2d.start(MiniJump2d.builtin_opts.word_start)<CR>",
+    { silent = true, desc = "Jump2d início de palavra" }
+)
+
+vim.keymap.set(
+    modes,
+    "<leader>jl",
+    "<Cmd>lua MiniJump2d.start(MiniJump2d.builtin_opts.line_start)<CR>",
+    { silent = true, desc = "Jump2d início de linha" }
+)
+
+vim.keymap.set(
+    modes,
+    "<leader>jc",
+    "<Cmd>lua MiniJump2d.start(MiniJump2d.builtin_opts.single_character)<CR>",
+    { silent = true, desc = "Jump2d caractere" }
+)
+
+vim.keymap.set(
+    modes,
+    "<leader>jq",
+    "<Cmd>lua MiniJump2d.start(MiniJump2d.builtin_opts.query)<CR>",
+    { silent = true, desc = "Jump2d consulta" }
+)
+
+vim.keymap.set(modes, "<leader>jd", "<Cmd>lua MiniJump2d.start()<CR>", {
+    silent = true,
+    desc = "Jump2d pontos padrão",
 })
